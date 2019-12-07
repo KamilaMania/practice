c3cd2407f2af134d0c9dd85296519f1ae523cab1
KamilaMania <mania.kamika@icloud.com>
2019-12-07 12:48:24 +0100

    msg-api updated and finished

--- messages-api.js
+++ messages-api.js
@@ -9,9 +9,34 @@ app.use(corsMiddleware);
 const parserMiddleware = bodyParser.json();
 app.use(parserMiddleware);
 
+let count = 0;
+
+const countMiddleware = function(req, res, next) {
+  console.log(req.originalUrl);
+  if (count <= 5 && req.originalUrl === "/messages") {
+    req.allowedRequest = true;
+    count++;
+  } else if (count > 5 && req.originalUrl === "/messages") {
+    req.allowedRequest = false;
+    res.status(429).send({
+      message: "Too many requests, try again tomorrow"
+    });
+  }
+  next();
+};
+
+app.use(countMiddleware);
+
 app.post("/messages", (req, res) => {
-  console.log("Your text was:" + req.body.text);
-  res.json({ message: "Message received loud and clear" });
+  if (req.body.text == null) {
+    res.status(400).send({
+      message: "Please supply text parameter"
+    });
+  }
+  if (req.allowedRequest) {
+    console.log("Your text was:" + req.body.text);
+    res.json({ message: "Message received loud and clear" });
+  }
 });
 
 const port = 3000;
