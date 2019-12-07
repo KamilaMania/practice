ba32aa0e4dd3aa73f1319eccbd758035a3ef452f
KamilaMania <mania.kamika@icloud.com>
2019-12-07 11:31:57 +0100

    post endpoint

--- messages-api.js
+++ messages-api.js
@@ -1,6 +1,14 @@
 const express = require("express");
-
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 
 const port = 3000;
 app.listen(port, () => console.log("listening on port 3000!"));
+app.post("/messages", (req, res) => {
+  console.log("Your text was:" + req.body.text);
+  res.json({ message: "Message received loud and clear" });
+});
+
+const parserMiddleware = bodyParser.json();
+app.use(parserMiddleware);
