cd3621302ca44029a2faab69df5bea7b89741dcd
KamilaMania <mania.kamika@icloud.com>
2019-12-07 11:47:15 +0100

    returning msg, getting req

--- messages-api.js
+++ messages-api.js
@@ -3,12 +3,16 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
 
-const port = 3000;
-app.listen(port, () => console.log("listening on port 3000!"));
+const corsMiddleware = cors();
+app.use(corsMiddleware);
+
+const parserMiddleware = bodyParser.json();
+app.use(parserMiddleware);
+
 app.post("/messages", (req, res) => {
   console.log("Your text was:" + req.body.text);
   res.json({ message: "Message received loud and clear" });
 });
 
-const parserMiddleware = bodyParser.json();
-app.use(parserMiddleware);
+const port = 3000;
+app.listen(port, () => console.log("listening on port 3000!"));
